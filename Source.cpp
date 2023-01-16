789f22a20fc1d439ed2ab288c8c9c94d475e8aaa
kenji <2605401+kenjinote@users.noreply.github.com>
2023-01-16 23:09:04 +0900

    プロジェクト ファイルを追加します。

---
+++ Source.cpp
@@ -0,0 +1,81 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+
+TCHAR szClassName[] = TEXT("closewithmiddlebutton");
+
+LRESULT CALLBACK LowLevelMouseProc(
+	_In_ int    nCode,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+) {
+	if (wParam == WM_MBUTTONUP) {
+		POINT point = {};
+		GetCursorPos(&point);
+		HWND hWnd = WindowFromPoint(point);
+		if (hWnd && HTCAPTION == SendMessage(hWnd, WM_NCHITTEST, 0, MAKELPARAM(point.x, point.y))) {
+			hWnd = GetAncestor(hWnd, GA_ROOT);
+			PostMessage(hWnd, WM_CLOSE, 0, 0);
+		}
+	}
+	return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HHOOK hook;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, NULL, 0);
+		break;
+	case WM_DESTROY:
+		UnhookWindowsHookEx(hook);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nShowCmd
+) {
+	MSG msg;
+	WNDCLASS wndclass = {
+		0,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		0,
+		0,
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		0,
+		WS_OVERLAPPEDWINDOW,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
